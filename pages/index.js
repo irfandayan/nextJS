29f08f3e6882b2595024d1659da7a210e6c10070
irfan <engr.irfandayan@gmail.com>
2022-09-30 19:08:04 +0500

    add layout, 404 & global styling

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,27 @@ import Image from 'next/image';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <h1>Home Page</h1>
-      <p>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>
         It is a long established fact that a reader will be distracted by the
         readable content of a page when looking at its layout. The point of
         using Lorem Ipsum is that it has a more-or-less normal distribution of
         letters.
       </p>
-      <p>
+      <p className={styles.text}>
         It is a long established fact that a reader will be distracted by the
         readable content of a page when looking at its layout. The point of
         using Lorem Ipsum is that it has a more-or-less normal distribution of
         letters.
       </p>
-      <Footer />
+      <Link href="/ninja">
+        <a className={styles.btn}>See Ninja Listing</a>
+      </Link>
     </div>
   );
 }
