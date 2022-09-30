065eb14498419f8a375fd4831f7f485a049fa6e2
irfan <engr.irfandayan@gmail.com>
2022-09-30 19:34:25 +0500

    Add images & meta data

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,27 @@
+import Head from 'next/head';
 const about = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters.
-      </p>
-      <p>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title> Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters.
+        </p>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,29 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Home Page</h1>
-      <p className={styles.text}>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters.
-      </p>
-      <p className={styles.text}>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters.
-      </p>
-      <Link href="/ninja">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title> Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home Page</h1>
+        <p className={styles.text}>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters.
+        </p>
+        <p className={styles.text}>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters.
+        </p>
+        <Link href="/ninja">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
